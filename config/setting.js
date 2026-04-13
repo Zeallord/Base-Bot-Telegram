1a39450cce7836e94633280beeea953c9a8ccaa5
zeallord <143248947+Zeallord@users.noreply.github.com>
2026-04-13 15:08:50 +0700

    Create setting.js

---
+++ config/setting.js
@@ -0,0 +1,5 @@
+//DATA OWNER
+global.OWNER_BOT = "xxxxx"; // ISI NAMA CREATOR BOT
+global.ID_OWNER_BOT = "xxxxxx"; // ISI ID TELEGRAM OWNER
+global.ID_ADMIN_BOT = [""]; // ISI PARA ID ADMIN TELEGRAM (OPSIONAL) = Bisa lebih dari 1
+global.TELE_BOT_TOKEN = "xxxxxx"; // ISI TOKEN BOT TELEGRAM
